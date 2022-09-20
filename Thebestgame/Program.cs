370437eafb1cf5fe533c651366117a058dbe45de
Robertspoon <robertthemcjug@gmail.com>
2022-09-20 10:20:04 -0300

    Initial commit

---
+++ Thebestgame/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thebestgame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Super Awesome Cool Game");
+            Console.WriteLine();
+
+            int lives;
+            lives = 3;
+            Console.WriteLine("Lives: " + lives);
+            Console.ReadKey(true);
+        }
+    }
+}
